510229164d240e7f70ebed51f86f1c88df5cda2d
isaacgilmore <isaac.l.gilmore@gmail.com>
2020-01-31 23:17:47 -0600

    todos, delete, complete, fireworks

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,96 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <div v-if="fireworks" class="pyro">
+    <div class="before"></div>
+    <div class="after"></div>
   </div>
+  <h2 id="main-title">todos</h2>
+  <el-row type="flex" justify="center">
+    <el-col :span="18">
+      <el-input class="input" placehold="What needs to be done?" v-model="newTodo" @keyup.enter.native="addTodo">
+        <el-button slot="append" icon="el-icon-check" @click="addTodo"></el-button>
+      </el-input>
+    </el-col>
+  </el-row>
+  <app-todo v-for="(todo, index) in todos" :todoName="todo.name" :key="index" @todo-deleted="deleteTodo" @todo-completed="completeTodo">
+  </app-todo>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTodo from './components/Todo.vue';
 
 export default {
-  name: 'app',
+  data() {
+    return {
+      newTodo: '',
+      todoTotal: '',
+      fireworks: false,
+      todos: [{
+          name: 'Emails',
+          completed: false,
+          id: 1
+        },
+        {
+          name: 'Other Stuff',
+          completed: false,
+          id: 2
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo) {
+        this.todos.unshift({
+          name: this.newTodo,
+          completed: false,
+          id: this.todos.length + 1
+        });
+        this.newTodo = '';
+      }
+    },
+    deleteTodo(todoName) {
+      window.console.log(todoName);
+      for (let i = 0; i < this.todos.length; i++) {
+        if (todoName === this.todos[i].name) {
+          this.todos.splice(i, 1);
+        }
+      }
+    },
+    openMessage(todoName) {
+      this.$alert('You completed ' + todoName, 'Yay!', {
+         confirmButtonText: 'OK'
+      }).then(()=>{this.fireworks = false});
+    },
+    completeTodo(todoName) {
+      for (let i = 0; i < this.todos.length; i++) {
+        if (todoName === this.todos[i].name) {
+          this.todos.splice(i, 1);
+        }
+        this.openMessage(todoName);
+        this.fireworks = true;
+      }
+    }
+  },
   components: {
-    HelloWorld
+    'app-todo': AppTodo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css?family=Anonymous+Pro&display=swap');
+@import './plugins/fireworks.css';
+body {
+  font-family: 'Anonymouse Pro', monospace;
+}
+
+#main-title {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 60pt;
+  font-weight: bolder;
+  color: lightcoral;
+  margin-bottom: 5px;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-row type="flex" justify="center" class="row-container">
+      <el-col :span="16" class="row-todo">
+        <el-button @click="completeTodo" type="success" plain size="small" icon="el-icon-check" circle></el-button>
+        {{ todoName }}
+        <!-- <el-button-group class="button-group"> -->
+          <!-- <el-button type="primary" plain size="small" icon="el-icon-edit" circle></el-button> -->
+          <el-button @click="deleteTodo" class="delete-btn" type="danger" plain size="small" icon="el-icon-close" circle></el-button>
+        <!-- </el-button-group> -->
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['todoName', 'completed'],
+    methods: {
+      deleteTodo() {
+        window.console.log('hit del');
+        window.console.log(this.todoName);
+        this.$emit('todo-deleted', this.todoName);
+      },
+      completeTodo() {
+        this.$emit('todo-completed', this.todoName);
+      }
+    }
+  }
+</script>
+
+<style>
+  .row-todo {
+    background: lightblue;
+    border-radius: 6px;
+    margin: 3px;
+    padding: 5px;
+    line-height: 2em;
+  }
+  .row-todo:hover {
+    background: #2343ff;
+  }
+  .delete-btn {
+    float: right;
+  }
+
+</style>
